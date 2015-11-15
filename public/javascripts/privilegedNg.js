9e250e4eb8ae79a8310bd01a77b08796204b1e4c
neeraj-gupta <neeraj.gupta@sjsu.edu>
2015-11-14 19:30:25 -0800

    Home page sections and css update

--- public/javascripts/privilegedNg.js
+++ public/javascripts/privilegedNg.js
@@ -1,4 +1,4 @@
-var app = angular.module('myApp', ['ui.router', 'ngMaterial']);
+var app = angular.module('myApp', ['fullPage.js','ui.router', 'ngMaterial']);
 
 app.config(['$stateProvider', '$urlRouterProvider',
     function($stateProvider, $urlRouterProvider){
